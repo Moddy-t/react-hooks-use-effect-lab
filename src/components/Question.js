5b8a7f2c3d90ec870461d5891ba99c7635aacb4f
Moddy-t <amalembamodesta@gmail.com>
2024-08-18 14:04:12 +0300

    useEffect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-function Question({ question, onAnswered }) {
+function Question({ onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(prevTime => {
+        if (prevTime <= 1) {
+          onAnswered(false);
+          return 10; // Reset timer
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
-
-  const { id, prompt, answers, correctIndex } = question;
+    return () => clearTimeout(timer); // Cleanup function
+  }, [timeRemaining, onAnswered]);
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <h1>Question</h1>
+      <p>{timeRemaining} seconds remaining</p>
+      {/* Render question and possible answers here */}
+    </div>
   );
 }
 
